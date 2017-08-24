f439e074f5a43a003acd8c0d1bab1546346ccffb
Nick <blizzard@gmail.com>
2017-08-24 08:54:23 -0400

    Got a working version of an endpoint call

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
 	"strconv"
 )
 
@@ -13,17 +13,38 @@ const (
 	bufferSize = 1024
 )
 
+type name struct {
+	First string
+	Last  string
+}
+
 func main() {
-	http.HandleFunc("/test", testHandler)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	http.HandleFunc("/Name", nameHandler)
+	http.HandleFunc("/NameValue", nameValueHandler)
 
 	panic(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadFile("test.html")
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadFile("name.html")
 	if err != nil {
-		http.Error(w, "Could not find test.html file.", http.StatusInternalServerError)
+		http.Error(w, "Could not find name.html file.", http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprintf(w, "%s", body)
+}
+
+func nameValueHandler(w http.ResponseWriter, r *http.Request) {
+	nameValue := name{First: "Nick", Last: "Blizard"}
+
+	json, err := json.Marshal(nameValue)
+	if err != nil {
+		http.Error(w, "Unable to format JSON response.", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
