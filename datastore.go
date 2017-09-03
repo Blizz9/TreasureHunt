8154bd28a5c087428d75fba6f29a1f2fa16518fc
Nick <blizzard@gmail.com>
2017-09-03 11:29:18 -0400

    Some code changes, and implementation of docker and aws files

--- datastore.go
+++ datastore.go
@@ -12,83 +12,7 @@ import (
 
 const availabilityZoneName = "us-east-1"
 
-type wsMessage struct {
-	Username  string
-	Timestamp int64
-	Message   string
-}
-
-type shrine struct {
-	ShrineID     string
-	Latitude     float64 `json:"Latitude,string"`
-	Longitude    float64 `json:"Longitude,string"`
-	ShrineNumber int
-	ShrineType   int
-}
-
-type shrineCheck struct {
-	UserID    string
-	Timestamp int64
-	Latitude  float64 `json:"Latitude,string"`
-	Longitude float64 `json:"Longitude,string"`
-}
-
-func storeMessage(message wsMessage) {
-	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String(availabilityZoneName)}))
-
-	writeInput := &dynamodb.BatchWriteItemInput{
-		RequestItems: map[string][]*dynamodb.WriteRequest{
-			"GoChat": {
-				&dynamodb.WriteRequest{
-					PutRequest: &dynamodb.PutRequest{
-						Item: map[string]*dynamodb.AttributeValue{
-							"Username": {
-								S: aws.String(message.Username),
-							},
-							"Timestamp": {
-								N: aws.String(strconv.FormatInt(message.Timestamp, 10)),
-							},
-							"Message": {
-								S: aws.String(message.Message),
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	_, err := svc.BatchWriteItem(writeInput)
-	if err != nil {
-		fmt.Println("Unable to properly write to database.", err)
-		return
-	}
-}
-
-func retreiveMessages() []wsMessage {
-	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String(availabilityZoneName)}))
-
-	scanInput := &dynamodb.ScanInput{
-		TableName: aws.String("GoChat"),
-	}
-
-	scan, err := svc.Scan(scanInput)
-	if err != nil {
-		fmt.Println("Unable to scan database.", err)
-		return nil
-	}
-
-	messages := []wsMessage{} // notice db schema matches messages format, convention over configuration
-	err = dynamodbattribute.UnmarshalListOfMaps(scan.Items, &messages)
-	if err != nil {
-		fmt.Println("Unable to parse database items.", err)
-		return nil
-	}
-
-	return messages
-}
-
-func retreiveShrines() []shrine {
+func retreiveShrines() []Shrine {
 	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String(availabilityZoneName)}))
 
 	scanInput := &dynamodb.ScanInput{
@@ -101,7 +25,7 @@ func retreiveShrines() []shrine {
 		return nil
 	}
 
-	shrines := []shrine{}
+	shrines := []Shrine{}
 	err = dynamodbattribute.UnmarshalListOfMaps(scan.Items, &shrines)
 	if err != nil {
 		fmt.Println("Unable to parse database items.", err)
@@ -111,7 +35,7 @@ func retreiveShrines() []shrine {
 	return shrines
 }
 
-func storeShrineCheck(shrineCheck shrineCheck) {
+func storeLocationQuery(locationQuery LocationQuery) {
 	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String(availabilityZoneName)}))
 
 	writeInput := &dynamodb.BatchWriteItemInput{
@@ -121,16 +45,16 @@ func storeShrineCheck(shrineCheck shrineCheck) {
 					PutRequest: &dynamodb.PutRequest{
 						Item: map[string]*dynamodb.AttributeValue{
 							"UserID": {
-								S: aws.String(shrineCheck.UserID),
+								S: aws.String(locationQuery.UserID),
 							},
 							"Timestamp": {
-								N: aws.String(strconv.FormatInt(shrineCheck.Timestamp, 10)),
+								N: aws.String(strconv.FormatInt(locationQuery.Timestamp, 10)),
 							},
 							"Latitude": {
-								N: aws.String(strconv.FormatFloat(shrineCheck.Latitude, 'f', -1, 64)),
+								N: aws.String(strconv.FormatFloat(locationQuery.Latitude, 'f', -1, 64)),
 							},
 							"Longitude": {
-								N: aws.String(strconv.FormatFloat(shrineCheck.Longitude, 'f', -1, 64)),
+								N: aws.String(strconv.FormatFloat(locationQuery.Longitude, 'f', -1, 64)),
 							},
 						},
 					},
